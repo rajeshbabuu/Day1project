98b4cfb0349a1ecb6f10e8e7b44ba01e2c1b4a00
Rajesh kumar <rajeshbabu656656.com>
2022-10-11 15:55:34 +0530

    UPDATE: Take Input From User

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace bridgelabzsession2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+
         }
     }
 }
