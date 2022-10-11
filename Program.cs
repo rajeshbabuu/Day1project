d340f5930d8ed32f20298ee85c1e1f2b09f4808c
Rajesh kumar <rajeshbabu656656.com>
2022-10-11 14:42:34 +0530

    printing Hello-World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bridgelabzsession2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
